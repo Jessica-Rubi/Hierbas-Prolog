e690776b3934b66873b88e01e63751d5175b9b1e
Jessica-Rubi <73010466+Jessica-Rubi@users.noreply.github.com>
2021-03-04 09:05:07 -0600

    Add files via upload

---
+++ Hierbas.pl
@@ -0,0 +1,111 @@
+afrodisiaca(excita_apetito_sexual).
+analgesica(quita_dolor).
+anestesica(insensibiliza_partes_del_cuerpo).
+antidiarreica(controla_diarreas).
+antiespamodica(controla_espasmos).
+antiflogistica(contra_inflamaciones).
+antipiretica(quita_fiebre).
+antiseptica(mata_tejidos).
+aperitiva(produce_apetito).
+astringente(contraer_tejidos).
+carminativa(evita_gases).
+colagoga(expulsa_bilis).
+depurativa(limpia_sangre).
+diaforetica(provoca_sudar).
+digestiva(favorece_digestion).
+diuretica(provoca_orina).
+emetica(provoca_nauseas_vomitos).
+emenagoga(activa_menstruacion).
+estupefaciente(aquieta_duerme_atonta).
+expectorante(expulsa_flemas_mucosidades).
+hemostatica(detiene_hemorragias).
+hepatica(ayuda_higado).
+laxante(purga_sin_diarrea).
+pectoral(ayuda_pecho).
+sedante(calma_dolores_intestinales).
+tonica(da_fuerza_organismo).
+toxica(venenosa).
+vermifuga(expulsa_gusanos_intestinales).
+vulneraria(cura_llagas_heridas).
+
+enfermedad(anemia).
+enfermedad(colicos).
+enfermedad(nauseas).
+enfermedad(tifoidea).
+enfermedad(flatulencias).
+enfermedad(digestion_mala).
+enfermedad(congestiones).
+enfermedad(fiebres_malignas).
+enfermedad(bronquitis).
+enfermedad(reumas).
+enfermedad(bichos_intestinales).
+enfermedad(flemas).
+enfermedad(tos).
+enfermedad(hidropesia).
+enfermedad(hemorragias_internas).
+enfermedad(disenteria).
+enfermedad(enteritis).
+enfermedad(impotencia_sexual).
+enfermedad(pulmonia).
+enfermedad(caspa).
+enfermedad(sarampion).
+enfermedad(vejija).
+
+forma_uso(cocimiento).
+forma_uso(infusion).
+forma_uso(maceracion).
+forma_uso(jarabe).
+forma_uso(tintura).
+forma_uso(jugo).
+forma_uso(horchata).
+
+plantas(canela).
+plantas(cedron).
+plantas(cardo_santo).
+plantas(nuez_vomica).
+plantas(ocote).
+plantas(ortiga).
+
+propiedades(canela, tonico).
+propiedades(cedron, tonico_nervioso).
+propiedades(cardo_santo, toxica).
+propiedades(nuez_vomica, emetica).
+propiedades(nuez_vomica, febrifuga).
+propiedades(ortiga, pectoral).
+propiedades(ortiga, astringente).
+propiedades(ortiga, diuretica).
+
+no_usar(cedron, niños).
+no_usar(cedron, convalecientes).
+no_usar(cardo_santo, via_oral).
+
+trata(canela, anemia, infusion).
+trata(canela, digestion, infusion).
+trata(canela, gases, infusion).
+trata(canela, tifoidea, infusion).
+trata(cedron, colicos, cocimiento).
+trata(cedron, nauseas, cocimiento).
+trata(cedron, flatulencias, cocimiento).
+trata(cedron, digestion_mala, cocimiento).
+trata(cedron, disenteria, cocimiento).
+trata(cedron, enteritis, cocimiento).
+trata(cedron, tos, cocimiento).
+trata(cardo_santo, ojos, cocimiento).
+trata(nuez_vomica, congestiones, infusion).
+trata(nuez_vomica, fiebres_malignas, infusion).
+trata(nuez_vomica, impotencia_sexual, infusion).
+trata(ocote, bronquitis, cocimiento).
+trata(ocote, reumas, cocimiento).
+trata(ocote, bichos_intestinales, cocimiento).
+trata(ocote, pulmonia, cocimiento).
+trata(ortiga, flemas, n/a).
+trata(ortiga, purifuca_sangre, n/a).
+trata(ortiga, tos, n/a).
+trata(ortiga, hidropesia, n/a).
+trata(ortiga, hemorragias_internas, n/a).
+trata(ortiga, reumas, n/a).
+trata(ortiga, cabello, n/a).
+trata(ortiga, caspa, n/a).
+trata(ortiga, sarampion, n/a).
+trata(ortiga, vejija, n/a).
+
